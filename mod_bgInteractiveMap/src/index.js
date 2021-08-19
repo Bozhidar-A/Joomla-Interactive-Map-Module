0342044df8f426b6a49620acc78885def524dac9
Bozhidar Atanasov <bplatanasov@gmail.com>
2021-08-19 11:58:27 +0200

    made module easier to edit

--- mod_bgInteractiveMap/src/index.js
+++ mod_bgInteractiveMap/src/index.js
@@ -2,23 +2,16 @@ import L from '../node_modules/leaflet/dist/leaflet';
 import '../node_modules/leaflet/dist/leaflet.css';
 import ZoomMarker from './ZoomMarker';
 
-/* This code is needed to properly load the images in the Leaflet CSS */
-// delete L.Icon.Default.prototype._getIconUrl;
-// L.Icon.Default.mergeOptions({
-//   iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
-//   iconUrl: require('leaflet/dist/images/marker-icon.png'),
-//   shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
-// });
 
 const defaultCenter = [42.7249925, 25.4833039];
-const SOFIA_GPS = [42.6977,23.3219]
 const defaultZoom = 7.7;
+maxBoundsTopLeft = [44.267552653898946, 21.19562785223469];
+maxBoundsBottomRight = [41.24442015186197, 29.71685444362345]
 
 const map = L.map('map', {
   zoomSnap: 0.1, //updates leaflet to allow fractional zoom
   minZoom: defaultZoom, //user cant zoom out anymore then on start
-  maxBounds: [[44.267552653898946, 21.19562785223469], [41.24442015186197, 29.71685444362345]]
-  //forces map to stay on Bulgaria
+  maxBounds: [maxBoundsTopLeft, maxBoundsBottomRight] //forces map to stay between both
 });
 
 //tile map 
@@ -27,7 +20,7 @@ const basemap = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/service
   attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ, TomTom, Intermap, iPC, USGS, FAO, NPS, NRCAN, GeoBase, Kadaster NL, Ordnance Survey, Esri Japan, METI, Esri China (Hong Kong), and the GIS User Community'
 });
 
-//starts map on Bulgaria on defaultZoom zoom
+//starts map on bounds on defaultZoom zoom
 map.setView(defaultCenter, defaultZoom);
 
 //sets tiles to map object
